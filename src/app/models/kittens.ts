5491436ee2014900e3c8c27a7f1484b8c5c53935
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-11-22 13:52:54 +0100

    create kitten interface

---
+++ src/app/models/kittens.ts
@@ -0,0 +1,10 @@
+export interface Kitten {
+    id: number | null;
+    name: string;
+    age: number;
+    color: string;
+    race: string;
+    genre: string;
+    imageUrl: string;
+    isAdopted: boolean;
+  }
